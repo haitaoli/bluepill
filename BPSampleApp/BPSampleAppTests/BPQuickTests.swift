0db1245a0f702d004e5465a703962bcf2228dc62
Haitao Li <haitao.li@airbnb.com>
2017-06-19 10:05:24 -0700

    Add Quick

---
+++ BPSampleApp/BPSampleAppTests/BPQuickTests.swift
@@ -0,0 +1,19 @@
+//
+//  BPQuickTests.swift
+//  BPSampleApp
+//
+//  Created by haitao_li on 6/19/17.
+//  Copyright © 2017 LinkedIn. All rights reserved.
+//
+
+import Foundation
+
+import Nimble
+import Quick
+
+class BPQuickTests: QuickSpec {
+  override func spec() {
+    it("should show up in test list") {
+    }
+  }
+}
---
+++ Podfile
@@ -0,0 +1,14 @@
+platform :ios, '9.0'
+use_frameworks!
+workspace "Bluepill.xcworkspace"
+
+target "BPSampleApp" do
+  project "BPSampleApp/BPSampleApp.xcodeproj"
+
+  target "BPSampleAppTests" do
+    pod 'Quick'
+    pod 'Nimble'
+  end
+
+end
+
